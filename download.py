6eec778f3e5fbf07c67921d2e3c573b78b83ffd5
Gema Parreño <gema.parreno.piqueras@gmail.com>
2018-05-02 11:22:05 +0200

    Download  generator and download data program
    Gives the option on executing the program from terminal

---
+++ download.py
@@ -0,0 +1,12 @@
+import nltk
+from nltk.corpus import gutenberg
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download()
